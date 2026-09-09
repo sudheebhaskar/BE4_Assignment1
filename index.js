06647e09c7e1dc3b8b3200b69200aa295a2cf30d
sudheebhaskar <sudheebhaskar@gmail.com>
2026-09-09 12:27:02 +0530

    fix backend

--- index.js
+++ index.js
@@ -364,10 +364,4 @@ app.delete("/books/:bookId", async (req, res) => {
 
 
 // START SERVER
-
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
